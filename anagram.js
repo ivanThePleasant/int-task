f3f66f18026153eaad4a03683dd88a0428977843
ivanThePleasant <ivanthepleasant.dev@gmail.com>
2025-03-20 09:00:58 +0200

    Initial commit

---
+++ anagram.js
@@ -0,0 +1,9 @@
+/**
+ Write a function that takes two words as an argument and returns true if they are anagrams (contain the exact same letters) and false otherwise.
+ 
+ An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ Examples:
+  Replays… Parsley
+  Leaf…Flea
+  Admirer… Married
+*/
